db499551531d8359770dcdb53929faef226a7d13
Sam Revanoor <sam@trint.com>
2021-06-05 15:22:58 -0400

    completes exercise 3

--- exercises/exercise-3.py
+++ exercises/exercise-3.py
@@ -10,3 +10,12 @@
 #      The dog's age in dog years is xx
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
+
+age = int(input("Input a dog's age in human years: "))
+
+if age <= 2:
+    print(f"The dog's age in dog years is {age * 10}")
+elif age > 2:
+    excluding_first_two = age - 2
+    dog_years = (2 * 10) + (excluding_first_two * 7)
+    print(f"The dog's age in dog years is {dog_years}")
