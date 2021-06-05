26f520cd35355705b9d134e98bd9fb584b15b762
Sam Revanoor <sam@trint.com>
2021-06-05 15:16:04 -0400

    completes exercise 2

--- exercises/exercise-2.py
+++ exercises/exercise-2.py
@@ -7,3 +7,8 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+phrase = input('Please enter a word or phrase: ').lower()
+while phrase != 'quit':
+    print(f'What you entered is {len(phrase)} characters long')
+    phrase = input('Please enter a word or phrase: ').lower()
+print('Goodbye!')
